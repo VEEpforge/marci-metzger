e3639bc69af06dbbbcae3637bc7a17977163ec71
Ezra Earl Villanueva <ezraearlvillanueva@gmail.com>
2025-10-24 01:37:51 +0800

    Add background transitions

--- app/(home)/underlay.tsx
+++ app/(home)/underlay.tsx
@@ -1,17 +1,41 @@
+'use client'
+
 import Image from 'next/image'
-import background from '../../assets/background.jpg'
-import React from 'react'
+import Autoplay from 'embla-carousel-autoplay'
+import Fade from 'embla-carousel-fade'
+import { background_images } from '@/assets'
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
 
 const Underlay = () => {
   return (
     <div className='min-h-screen min-w-screen fixed inset-0 z-0'>
       <div className='absolute inset-0 bg-black opacity-50 z-10'></div>
-      <Image
-        src={background}
-        alt='Description of image'
-        layout='fill'
-        objectFit='cover'
-      />
+      <Carousel
+        // plugins={[plugin.current]}
+        plugins={[
+          Autoplay({
+            delay: 5000,
+          }),
+          Fade(),
+        ]}
+        opts={{ loop: true }}
+      >
+        <CarouselContent>
+          { background_images.map((image, index) => (
+            <CarouselItem key={index}>
+              <div className="relative h-screen w-full">
+              <Image
+                src={image}
+                alt='Description of image'
+                // layout='fill'
+                fill={true}
+                objectFit='cover'
+              />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   )
 }
---
+++ assets/index.ts
@@ -0,0 +1,9 @@
+import background_1 from './background-1.jpg';
+import background_2 from './background-2.jpg';
+import background_3 from './background-3.jpg';
+
+export const background_images = [
+  background_1,
+  background_2,
+  background_3
+];
